2f20c4dc9f635777b7895a4f261dafff87d71905
Max_Razzhivin <max.nvo06@gmail.com>
2025-05-31 09:06:19 +0300

    fix for pep8

--- conftest.py
+++ conftest.py
@@ -1,21 +1,23 @@
 import pytest
 from selene import browser
 
+
 @pytest.fixture(scope="session", autouse=True)
 def setting_browser():
     browser.driver.maximize_window()
     yield
     browser.quit()
 
+
 @pytest.fixture()
 def open_google():
     browser.open('https://google.com')
 
 
-
 @pytest.fixture()
 def open_duckgo():
     browser.open('https://duckduckgo.com/')
+
 
 @pytest.fixture()
 def open_niffler():
